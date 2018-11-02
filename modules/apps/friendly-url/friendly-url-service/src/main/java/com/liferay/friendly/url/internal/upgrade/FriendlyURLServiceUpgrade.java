12221d1930f64f3f1f045dc99539fbf14c056fb4
Jose A. Jimenez <jose.jimenez@liferay.com>
2018-11-02 12:35:39 +0100

    LPS-86289 Register the upgrade process

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/upgrade/FriendlyURLServiceUpgrade.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/upgrade/FriendlyURLServiceUpgrade.java
@@ -11,35 +11,25 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+package com.liferay.friendly.url.internal.upgrade;
 
-package com.liferay.portal.background.task.internal.upgrade;
-
-import com.liferay.portal.background.task.internal.upgrade.v1_0_0.UpgradeBackgroundTask;
-import com.liferay.portal.background.task.internal.upgrade.v1_0_0.UpgradeKernelPackage;
-import com.liferay.portal.background.task.internal.upgrade.v1_0_0.UpgradeSchema;
-import com.liferay.portal.background.task.internal.upgrade.v2_0_0.util.BackgroundTaskTable;
+import com.liferay.friendly.url.internal.upgrade.v2_0_0.util.FriendlyURLEntryTable;
 import com.liferay.portal.kernel.upgrade.BaseUpgradeSQLServerDatetime;
-import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Cristina Rodríguez
+ * @author José Ángel Jiménez
  */
 @Component(immediate = true, service = UpgradeStepRegistrator.class)
-public class BackgroundTaskServiceUpgrade implements UpgradeStepRegistrator {
+public class FriendlyURLServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
 		registry.register(
-			"0.0.1", "0.0.2", new UpgradeSchema(), new UpgradeKernelPackage());
-
-		registry.register("0.0.2", "1.0.0", new UpgradeBackgroundTask());
-
-		registry.register(
 			"1.0.0", "2.0.0",
 			new BaseUpgradeSQLServerDatetime(
-				new Class<?>[] {BackgroundTaskTable.class}));
+				new Class<?>[] {FriendlyURLEntryTable.class}));
 	}
 
 }
