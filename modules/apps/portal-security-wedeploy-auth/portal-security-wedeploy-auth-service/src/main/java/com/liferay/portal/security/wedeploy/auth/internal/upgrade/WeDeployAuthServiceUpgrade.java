18f33ecdabad49cdc09e76ae65d2e0f3e5bb96b6
Jose A. Jimenez <jose.jimenez@liferay.com>
2019-01-15 12:26:24 +0100

    LPS-86289 Register the upgrade process

--- modules/apps/portal-security-wedeploy-auth/portal-security-wedeploy-auth-service/src/main/java/com/liferay/portal/security/wedeploy/auth/internal/upgrade/WeDeployAuthServiceUpgrade.java
+++ modules/apps/portal-security-wedeploy-auth/portal-security-wedeploy-auth-service/src/main/java/com/liferay/portal/security/wedeploy/auth/internal/upgrade/WeDeployAuthServiceUpgrade.java
@@ -12,36 +12,28 @@
  * details.
  */
 
-package com.liferay.mobile.device.rules.internal.upgrade;
+package com.liferay.portal.security.wedeploy.auth.internal.upgrade;
 
-import com.liferay.mobile.device.rules.internal.upgrade.v2_0_0.util.MDRActionTable;
-import com.liferay.mobile.device.rules.internal.upgrade.v2_0_0.util.MDRRuleGroupInstanceTable;
-import com.liferay.mobile.device.rules.internal.upgrade.v2_0_0.util.MDRRuleGroupTable;
-import com.liferay.mobile.device.rules.internal.upgrade.v2_0_0.util.MDRRuleTable;
 import com.liferay.portal.kernel.upgrade.BaseUpgradeSQLServerDatetime;
-import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.security.wedeploy.auth.internal.upgrade.v2_0_0.util.WeDeployAuthAppTable;
+import com.liferay.portal.security.wedeploy.auth.internal.upgrade.v2_0_0.util.WeDeployAuthTokenTable;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Matthew Tambara
+ * @author José Ángel Jiménez
  */
 @Component(immediate = true, service = UpgradeStepRegistrator.class)
-public class MDRServiceUpgrade implements UpgradeStepRegistrator {
+public class WeDeployAuthServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
-		registry.register("0.0.1", "1.0.0", new DummyUpgradeStep());
-
-		registry.register("1.0.0", "1.0.1", new DummyUpgradeStep());
-
 		registry.register(
-			"1.0.1", "2.0.0",
+			"1.0.0", "2.0.0",
 			new BaseUpgradeSQLServerDatetime(
 				new Class<?>[] {
-					MDRActionTable.class, MDRRuleGroupInstanceTable.class,
-					MDRRuleGroupTable.class, MDRRuleTable.class
+					WeDeployAuthAppTable.class, WeDeployAuthTokenTable.class
 				}));
 	}
 
